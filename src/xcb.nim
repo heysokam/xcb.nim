17d753d577a887b4e4fdae30f6908a3fbff2c6c4
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-07 22:12:33 +0200

    new: Exporter file for all modules

---
+++ src/xcb.nim
@@ -0,0 +1,9 @@
+#:___________________________________________________________
+#  xcb.nim  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0  :
+#:___________________________________________________________
+##! @fileoverview Cable connector to the XCB modules
+#____________________________________________________|
+{.passL:"-lxcb".}
+{.passL:"-lxcb-util".}
+import ./xcb/types      ; export types
+import ./xcb/connection ; export connection
