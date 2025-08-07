b364cfed4dc5780b60c88c28cff72228efd1c895
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-07 22:13:06 +0200

    new: Square Example skeleton (WIP)

---
+++ examples/square.nim
@@ -0,0 +1,40 @@
+#:___________________________________________________________
+#  xcb.nim  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0  :
+#:___________________________________________________________
+import pkg/xcb
+
+type Example = object
+  connection :xcb.Connection
+
+
+func init () :Example=
+  # Perform initialization routines.
+  result = Example()
+  # Connect to the X server.
+  result.connection = xcb.Connection.create()
+  result.connection.validate()
+  # Perform X-related initialization.
+  return result
+
+
+func update (example :Example) :void=
+  # While not finished:
+  # : Receive the next event from the X server.
+  # : Handle the event, possibly sending various drawing requests to the X server.
+  # : If the event was a quit message, exit the loop.
+  discard example
+
+
+func close (example :Example) :bool= true
+func term (example :var Example) :void=
+  # Close down the connection to the X server.
+  # Perform cleanup operations.
+  discard example
+
+
+when isMainModule:
+  echo "Hello Square"
+  var example = square.init()
+  while not example.close(): example.update()
+  example.term()
+
