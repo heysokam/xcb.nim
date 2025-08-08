24a51c40d64aad8f9698321138cc4e5c7187ada9
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-08 10:13:23 +0200

    new: `{.readonly.}`, `{.private.}` and `{.internal.}` pragmas

---
+++ src/xcb/internal/pragmas.nim
@@ -0,0 +1,6 @@
+#:___________________________________________________________
+#  xcb.nim  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0  :
+#:___________________________________________________________
+template readonly *{.pragma.}
+template private  *{.pragma.}
+template internal *{.pragma.}
